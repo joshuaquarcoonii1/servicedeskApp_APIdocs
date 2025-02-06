1a57f0837fa3ca69da879d8393f795de28711cb5
Joshua Quarcoo <78474714+joshuaquarcoonii1@users.noreply.github.com>
2025-02-06 11:33:41 +0000

    Update App.js

--- App.js
+++ App.js
@@ -757,7 +757,7 @@ app.post('/api/reports/:id/assign', async (req, res) => {
 
 
 
-const PORT =3000;
+const PORT =process.env.PORT ||3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
